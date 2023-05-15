7637aef5f241699f3c8b59af2922ec5d5c1b6fff
cybervoigt <cybervoigt@gmail.com>
2023-05-15 16:11:08 -0300

    comparing with class for to do the same...

--- example_array_filter.php
+++ example_array_filter.php
@@ -9,7 +9,7 @@ $list = [2,3,4,3,2,1];
 
 $item = 3;
 
-$result = array_filter($list, 
+$result1 = array_filter($list, 
     function($_item)
     {
         global $item;
@@ -21,10 +21,32 @@ $result = array_filter($list,
 );
 
 print_r($list);
-print_r($result);
+print_r($result1);
 
-$qty = count($result);
-echo "the number of ocurrences of {$item} inside the list is : {$qty}";
+$qty1 = count($result1);
+echo "1 - the number of ocurrences of {$item} inside the list is : {$qty1}\n\n";
+
+
+/**
+ * I think, in my opinion that a classic 'for' (or foreach)
+ *  can be better for reading the code, 
+ * as well as improving like the code below 
+ * where I don't create another array...
+ */
+
+$qty2 = 0;
+$q = count($list);
+for($i = 0; $i < $q; $i++)
+{
+    $_item = $list[$i];
+    if($_item == $item)
+    {
+        $qty2++;
+    }
+}
+
+echo "2 - the number of ocurrences of {$item} inside the list is : {$qty2}\n\n";
+
 
 
 ?>
